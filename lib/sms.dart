f0e3f63f7595881a4f33bc65c53545bf9b8ed69e
jonathanmajh <jonathanmajh@gmail.com>
2022-09-27 00:51:49 -0400

    sms background reading

---
+++ lib/sms.dart
@@ -0,0 +1,10 @@
+// import 'package:telephony/telephony.dart';
+
+// final Telephony telephony = Telephony.instance;
+
+// telephony.listenIncomingSms(
+//      onNewMessage: (SmsMessage message) {
+//          // Handle message
+//      },
+//      listenInBackground = false
+//  );
